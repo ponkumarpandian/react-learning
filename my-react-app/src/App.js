0249cad9421195593d0e63b3af0dc99c57d12e03
Pon Kumar P Thangapandi <pon.kumar.p.thangapandi@pwc.com>
2019-11-03 02:03:21 +0530

    Added navbar and lift up to state and sync muti comp

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import NavBar from "./components/navebar";
+import Products from "./components/products";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    products: [
+      {
+        producId: 10000,
+        productName: "product1",
+        productDescription: "product1",
+        price: 50,
+        quantity: 0
+      },
+      {
+        producId: 10001,
+        productName: "product2",
+        productDescription: "product2",
+        price: 200,
+        quantity: 0
+      },
+      {
+        producId: 10002,
+        productName: "product3",
+        productDescription: "product3",
+        price: 150,
+        quantity: 0
+      }
+    ]
+  };
+
+  handleReset = () => {
+    const products = [...this.state.products];
+    products.map(p => (p.quantity = 0));
+    this.setState({ products });
+  };
+
+  handleDelete = product => {
+    const products = this.state.products.filter(
+      p => p.producId !== product.producId
+    );
+    console.log(products);
+    this.setState({ products });
+  };
+
+  handleIncrement = product => {
+    const products = [...this.state.products]; //... clone the products object
+    const index = products.indexOf(product);
+    products[index] = { ...product }; //cloning the product to update
+    products[index].quantity = products[index].quantity + 1; //Increment the current prodcut quantity
+    this.setState({ products }); // Update the state
+  };
+
+  handleDecrement = product => {
+    const products = [...this.state.products]; //... clone the products object
+    const index = products.indexOf(product);
+    products[index] = { ...product }; //cloning the product to update
+    products[index].quantity = products[index].quantity - 1; //Decrement the current prodcut quantity
+    this.setState({ products }); // Update the state
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar
+          totalProducts={this.state.products.filter(p => p.quantity > 0).length}
+        />
+        <main className="container">
+          <Products
+            products={this.state.products}
+            onReset={this.handleReset}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- my-react-app/src/components/products.jsx
+++ my-react-app/src/components/products.jsx
@@ -3,32 +3,6 @@ import Counter from "./counter";
 import DeleteProduct from "./deleteProduct";
 
 class products extends Component {
-  state = {
-    products: [
-      {
-        producId: 10000,
-        productName: "product1",
-        productDescription: "product1",
-        price: 50,
-        quantity: 0
-      },
-      {
-        producId: 10001,
-        productName: "product2",
-        productDescription: "product2",
-        price: 200,
-        quantity: 0
-      },
-      {
-        producId: 10002,
-        productName: "product3",
-        productDescription: "product3",
-        price: 150,
-        quantity: 0
-      }
-    ]
-  };
-
   render() {
     return (
       <React.Fragment>
@@ -44,79 +18,55 @@ class products extends Component {
   }
 
   renderProducts() {
-    console.log("render products");
-    const { products } = this.state;
-    if (this.state.products.length === 0)
-      return <p>There is no product found! </p>;
+    const { products } = this.props;
+    if (products.length === 0) return <p>There is no products in cart! </p>;
     return (
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Product</th>
-            <th>Quntity</th>
-            <th>Unit Price</th>
-            <th>Total</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map(product => (
-            <tr key={product.producId}>
-              <td>{product.producId}</td>
-              <td>{product.productName}</td>
-              <td>
-                <Counter
-                  key={product.producId}
-                  product={product}
-                  onIncrement={this.handleIncrement}
-                  onDecrement={this.handleDecrement}
-                ></Counter>
-              </td>
-              <td>₹{product.price}</td>
-              <td>₹{product.price * product.quantity}</td>
-              <td>
-                <DeleteProduct
-                  key={product.producId}
-                  product={product}
-                  onDelete={this.handleDelete}
-                ></DeleteProduct>
-              </td>
+      <React.Fragment>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Product</th>
+              <th>Quntity</th>
+              <th>Unit Price</th>
+              <th>Total</th>
+              <th>Action</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {products.map(product => (
+              <tr key={product.producId}>
+                <td>{product.producId}</td>
+                <td>{product.productName}</td>
+                <td>
+                  <Counter
+                    key={product.producId}
+                    product={product}
+                    onIncrement={this.props.onIncrement} //bubling the event to parent component (app.js)
+                    onDecrement={this.props.onDecrement} //bubling the event to parent component (app.js)
+                  ></Counter>
+                </td>
+                <td>₹{product.price}</td>
+                <td>₹{product.price * product.quantity}</td>
+                <td>
+                  <DeleteProduct
+                    key={product.producId}
+                    product={product}
+                    onDelete={this.props.onDelete} //bubling the event to parent component (app.js)
+                  ></DeleteProduct>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <button
+          onClick={this.props.onReset} //bubling the event to parent component (app.js)
+          className="btn btn-secondary btn-sm"
+        >
+          Reset
+        </button>
+      </React.Fragment>
     );
-  }
-
-  handleDelete = product => {
-    const products = this.state.products.filter(
-      p => p.producId !== product.producId
-    );
-    console.log(products);
-    this.setState({ products });
-  };
-
-  handleIncrement = product => {
-    const products = [...this.state.products]; //... clone the products object
-    const index = products.indexOf(product);
-    products[index] = { ...product }; //cloning the product to update
-    products[index].quantity = products[index].quantity + 1; //Increment the current prodcut quantity
-    this.setState({ products }); // Update the state
-  };
-
-  handleDecrement = product => {
-    const products = [...this.state.products]; //... clone the products object
-    const index = products.indexOf(product);
-    products[index] = { ...product }; //cloning the product to update
-    products[index].quantity = products[index].quantity - 1; //Decrement the current prodcut quantity
-    this.setState({ products }); // Update the state
-  };
-
-  getBadgeClassNames() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.counter === 0 ? "warning" : "primary";
-    return classes;
   }
 }
 
